3d1e8a2ab6256db4dcf171190caf56017a5eb5eb
Aktarul Islam <aktarul857@gmail.com>
2018-05-22 21:49:41 +0600

    other options added

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 watermark = require('image-watermark');
 
 var options = {
-    text: 'sample watermark'
+    'text': 'sample watermark',
+    // 'change-format' : true,
+    // 'output-format' : 'bmp',
+    'color' : 'rgb(154, 50, 46)'
 };
 watermark.embedWatermark('SpiderMan.jpg',options);
 
